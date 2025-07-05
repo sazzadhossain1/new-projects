9f5b335f5d823db42b52a0f55f8d51a1a436fff8
sazzad tomal <sazzadtomal1@gmail.com>
2025-07-06 05:29:31 +0600

    second section responsive doen

--- src/components/SecondSection/SecondSection.js
+++ src/components/SecondSection/SecondSection.js
@@ -1,18 +1,6 @@
 import React from "react";
 import "./SecondSection.css";
 
-import photoOne from "../../accets/secondSectionPhoto/secondPhotoOne.webp";
-import photoTwo from "../../accets/secondSectionPhoto/secondPhotoTwo.webp";
-import photoThree from "../../accets/secondSectionPhoto/secondPhotoThird.webp";
-import photoFour from "../../accets/secondSectionPhoto/secondPhotoFour.webp";
-import photoFive from "../../accets/secondSectionPhoto/secondPhotoFive.webp";
-
-import SmallItems from "../../accets/secondSectionPhoto/SmallItems-removebg-preview.png";
-import LargeAndBulkyGoods from "../../accets/secondSectionPhoto/Large and bulky goods.png";
-import ValuablesAndDelicateGoods from "../../accets/secondSectionPhoto/Valuables and delicate goods.png";
-import MovingService from "../../accets/secondSectionPhoto/Moving service.png";
-import FoodAndGroceryDelivery from "../../accets/secondSectionPhoto/Food & grocery delivery.png";
-
 const SecondSection = () => {
   return (
     <div className="second_section_parent_div">
